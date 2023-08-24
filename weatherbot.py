741ccc7ebd7d96e10a0cd174ec5d73b8c6a2f62c
kezuh1ro <128370358+kezuh1ro@users.noreply.github.com>
2023-08-24 23:37:11 +0500

    Update weatherbot.py

--- weatherbot.py
+++ weatherbot.py
@@ -8,3 +8,8 @@ api = 'f4a8795027cd6ef2a3376afe27e3ac0d'
 @bot.message_handler(commands=['start'])
 def start(message):
   bot.send_message(message.chat.id, 'Привет!\nЭто бот для проверки погоды.\nПожалуйста напиши название города.')
+
+@bot.message_handler(content_types=['text'])
+def get_weather(message):
+  city_name = message.text.strip().lower()
+  url = requests.get(f'https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}')
