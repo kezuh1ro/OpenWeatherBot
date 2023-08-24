427fdcc0c915395d96329ca46ca7962f361ee18f
kezuh1ro <128370358+kezuh1ro@users.noreply.github.com>
2023-08-24 23:26:16 +0500

    Update weatherbot.py

--- weatherbot.py
+++ weatherbot.py
@@ -2,4 +2,9 @@ import telebot
 import requests
 import json
 
-bot = telebot.TeleBot('
+bot = telebot.TeleBot('6646999433:AAGDzncpqe-h3vZBFoJONRdh0IT_F5IRsKw')
+api = 'f4a8795027cd6ef2a3376afe27e3ac0d'
+
+@bot.message_handler(commands=['start'])
+def start(message):
+  bot.send_message(message.chat.id, 'Привет!\nЭто бот для проверки погоды.\nПожалуйста напиши название города.')
