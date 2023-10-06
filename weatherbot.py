e3f49d2129ccdbf826964ea78b255ef49c3ac745
kezuh1ro <128370358+kezuh1ro@users.noreply.github.com>
2023-10-06 19:03:58 +0500

    Update weatherbot.py

--- weatherbot.py
+++ weatherbot.py
@@ -2,7 +2,7 @@ import telebot
 import requests
 import json
 
-bot = telebot.TeleBot('6563583679:AAGnB7VYgwt-8XXV-oPaOv2B4Ok2aLVM4Oo')
+bot = telebot.TeleBot('6481200481:AAGnoR-AKYAeW9jPQ_FZuoGmq1-zZUUjTMs')
 api = 'f4a8795027cd6ef2a3376afe27e3ac0d'
 
 @bot.message_handler(commands=['start'])
